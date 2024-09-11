dc3ebfab3ef92a4ee91376053b9880c4ad954ae9
zolkins <57059372+zolkins@users.noreply.github.com>
2024-09-12 00:01:15 +0300

    update player movement, now independent of fps added initial logic for camera added sprites for NPCs updated gitignore

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,38 +1,35 @@
 extends CharacterBody2D
-var speed: float = 150.0
+var speed: float = 70.0
+var camera_zoom: Vector2 = Vector2(10, 10)
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$CameraPlayer.set_zoom(camera_zoom)
 
-
-func move_character_body() -> void:
-	var velocity = Vector2()
+func move_character_body(delta: float) -> void:
+	var velocity_player: Vector2 = Vector2()
 
 	if Input.is_action_pressed("ui_right"):
 		$AnimatedSprite2D.flip_h = false
-		velocity.x += 1
-		$AnimatedSprite2D.play("right")
+		velocity_player.x += 1
+	#		$AnimatedSprite2D.play("right")
 	if Input.is_action_pressed("ui_left"):
 		$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("right")
-		velocity.x -= 1
+		#		$AnimatedSprite2D.play("right")
+		velocity_player.x -= 1
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-		$AnimatedSprite2D.play("down")
+		velocity_player.y += 1
+	#		$AnimatedSprite2D.play("down")
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-		$AnimatedSprite2D.play("up")
+		velocity_player.y -= 1
+	#		$AnimatedSprite2D.play("up")
 
-	if velocity.length() == 0:
-		$AnimatedSprite2D.play("idle")
+	if velocity_player.length() == 0:
+		pass
+	#		$AnimatedSprite2D.play("idle")
 
-	velocity = velocity.normalized() * speed
-	set_velocity(velocity)
+	velocity_player = velocity_player.normalized() * speed * delta * 100.0
+	set_velocity(velocity_player)
 	move_and_slide()
 
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move_character_body()
+	move_character_body(delta)

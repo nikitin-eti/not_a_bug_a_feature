69a9b134ad16de06011e95d204a2179a0e665c5d
zolkins <57059372+zolkins@users.noreply.github.com>
2024-09-13 00:01:08 +0300

    added logic for enemy, edit tile_maps, changed resolution to 1920x1080

---
+++ Scripts/bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var velocity: Vector2 = Vector2()
+var duration: float = 2.5
+
+func _ready() -> void:
+	self.connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _process(delta: float) -> void:
+	position += velocity * delta
+
+	duration -= delta
+	if duration <= 0:
+		queue_free()
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("players"):
+		body.take_damage(1)
+		queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
-var speed: float = 70.0
+
+var speed: float = 50.0
 var camera_zoom: Vector2 = Vector2(10, 10)
+var current_health: int = 3 # Максимум 3 жизни
 
 func _ready() -> void:
+	add_to_group("players")
 	$CameraPlayer.set_zoom(camera_zoom)
+# Отображение жизней может быть реализовано через интерфейс (HUD), например через спрайты сердечек
 
 func move_character_body(delta: float) -> void:
 	var velocity_player: Vector2 = Vector2()
@@ -11,25 +15,32 @@ func move_character_body(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		$AnimatedSprite2D.flip_h = false
 		velocity_player.x += 1
-	#		$AnimatedSprite2D.play("right")
 	if Input.is_action_pressed("ui_left"):
 		$AnimatedSprite2D.flip_h = true
-		#		$AnimatedSprite2D.play("right")
 		velocity_player.x -= 1
 	if Input.is_action_pressed("ui_down"):
 		velocity_player.y += 1
-	#		$AnimatedSprite2D.play("down")
 	if Input.is_action_pressed("ui_up"):
 		velocity_player.y -= 1
-	#		$AnimatedSprite2D.play("up")
 
-	if velocity_player.length() == 0:
-		pass
-	#		$AnimatedSprite2D.play("idle")
-
-	velocity_player = velocity_player.normalized() * speed * delta * 100.0
+	velocity_player = velocity_player.normalized() * speed
 	set_velocity(velocity_player)
 	move_and_slide()
 
 func _process(delta: float) -> void:
 	move_character_body(delta)
+
+# Функция получения урона
+func take_damage(amount: int) -> void:
+	current_health -= amount # Уменьшаем количество жизней
+	if current_health <= 0:
+		current_health = 0
+		game_over() # Вызываем функцию проигрыша, если жизни закончились
+	else:
+		print("Player took damage. Current current_health:", current_health)
+
+# Функция проигрыша
+func game_over() -> void:
+	print("Game Over! Player has no current_health left.")
+	# Здесь можно добавить логику для завершения игры, перезагрузки уровня, показа экрана смерти и т.д.
+	queue_free() # Удаляем игрока из сцены (или можно перезапустить игру)

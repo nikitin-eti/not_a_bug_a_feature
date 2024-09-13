7198ed84d4f6eb4f70ee3ac52a0b94afdfe0620f
zolkins <57059372+zolkins@users.noreply.github.com>
2024-09-13 14:41:57 +0300

    end scene added

---
+++ Scripts/cutscene_end.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var video_player: VideoStreamPlayer = $VideoStreamPlayer
+@onready var sprite: Sprite2D = $Sprite
+
+func _ready() -> void:
+	sprite.hide()
+
+	await get_tree().create_timer(0.5).timeout
+	start_video()
+
+func start_video() -> void:
+	if video_player:
+		video_player.play()
+		video_player.finished.connect(_on_video_finished)
+	else:
+		print("VideoStreamPlayer не найден")
+
+func _on_video_finished() -> void:
+	sprite.show()

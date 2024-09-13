bff62da5a4bba3463737b334e4dc1d1fdaecade3
zolkins <57059372+zolkins@users.noreply.github.com>
2024-09-13 04:32:18 +0300

    added credits_2 scene

--- Scripts/credits.gd
+++ Scripts/credits.gd
@@ -3,3 +3,11 @@ extends Node
 #Switch to Start Menu when button is pressed
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/start_menu.tscn")
+
+
+func _on_next_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Credits_2.tscn")
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
